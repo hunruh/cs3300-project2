720ef155e45d2c8a8b2a040a0615052e27d47a27
Harrison Unruh <harrison.unruh@gmail.com>
2017-04-11 22:14:46 -0400

    Small tweak to bash script

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for number in {1956..2013}
+for number in {1956..2016}
 do
 curl "https://www.fema.gov/disasters/grid/year/csv/$number?field_disaster_type_term_tid_1=All&attach=page" -o "$number.csv"
 done
