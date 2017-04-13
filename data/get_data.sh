c8c428d9835fbd4681d0e273077d4e464b6dcb20
Harrison Unruh <harrison.unruh@gmail.com>
2017-04-12 22:46:31 -0400

    Repo reorganization

--- data/get_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for number in {1956..2016}
-do
-curl "https://www.fema.gov/disasters/grid/year/csv/$number?field_disaster_type_term_tid_1=All&attach=page" -o "$number.csv"
-done
-exit 0
